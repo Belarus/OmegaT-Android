bf2e667b41b4293f6ff3792e53d6a876ac82e38b
Alex Buloichik <alex73mail@gmail.com>
2011-07-25 08:24:13 +0300

    OmegaT 2.5

--- translate-Android.cmd
+++ translate-Android.cmd
@@ -1,1 +1,1 @@
-java -jar Android.OmegaT/OmegaT.jar --config-dir=Android.settings Android
+java -jar Android.OmegaT/OmegaT.jar --config-dir=Android.settings Android "--alternate-filename-from=_.+.xml$"  "--alternate-filename-to=_VERSION.xml"
