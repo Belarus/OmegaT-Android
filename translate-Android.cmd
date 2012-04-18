028720985557fb4c48c8dc0f86e6518b32fb5410
Alex Buloichik <alex73mail@gmail.com>
2012-04-18 09:32:08 +0300

    New OmegaT with automatic git support

--- translate-Android.cmd
+++ translate-Android.cmd
@@ -1,1 +1,1 @@
-java -jar Android.OmegaT/OmegaT.jar --config-dir=Android.settings Android "--alternate-filename-from=/.+.xml$"  "--alternate-filename-to=/"
+java -jar Android.OmegaT/OmegaT.jar --config-dir=Android.settings Android "--alternate-filename-from=/.+.xml$"  "--alternate-filename-to=/" --disable-project-locking
