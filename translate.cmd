16c2eb0e72632071f24d0070253f9bc7143f5716
Alex Buloichik <alex73mail@gmail.com>
2012-11-17 15:21:21 +0300

    Run OmegaT

---
+++ translate.cmd
@@ -0,0 +1,1 @@
+java -jar ../Common/OmegaT.binaries/OmegaT.jar --no-team "--alternate-filename-from=/.+.xml$"  "--alternate-filename-to=/" . --ITokenizer=org.omegat.plugins.tokenizer.SnowballEnglishTokenizer
---
+++ translate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar ../Common/OmegaT.binaries/OmegaT.jar --no-team "--alternate-filename-from=/.+.xml$"  "--alternate-filename-to=/" . --ITokenizer=org.omegat.plugins.tokenizer.SnowballEnglishTokenizer
