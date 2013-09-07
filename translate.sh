43b7b6ca357ddda828d8f7b9fa89367706387dfd
Yekhe Kim <yehekim@gmail.com>
2013-09-07 21:20:26 +0300

    Часова парамантавалі паламанае.

--- translate.sh
+++ translate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-java -jar ../Common/OmegaT.binaries/OmegaT.jar --no-team "--alternate-filename-from=/.+.xml$"  "--alternate-filename-to=/" . --ITokenizer=org.omegat.plugins.tokenizer.SnowballEnglishTokenizer resource-bundle=./Bundle_be.properties
+java -jar ../Common/OmegaT.binaries/OmegaT.jar --no-team "--alternate-filename-from=/.+.xml$"  "--alternate-filename-to=/" . --ITokenizer=org.omegat.plugins.tokenizer.SnowballEnglishTokenizer
+# resource-bundle=./Bundle_be.properties
