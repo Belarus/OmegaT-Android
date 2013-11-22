768e3bde732c2bd6e988f73c9a71e0c9c2b62567
Alex Buloichik <alex73mail@gmail.com>
2013-11-22 07:54:15 +0300

    run scripts

--- translate.cmd
+++ translate.cmd
@@ -1,1 +1,1 @@
-java -jar ../Common/OmegaT.binaries/OmegaT.jar --no-team "--alternate-filename-from=/.+.xml$"  "--alternate-filename-to=/" . --ITokenizer=org.omegat.plugins.tokenizer.SnowballEnglishTokenizer
+java -jar ../Common/OmegaT.binaries/OmegaT.jar --no-team "--alternate-filename-from=/.+.xml$"  "--alternate-filename-to=/" .
